847aa51002625a2dffce5c74166460d880294f23
Umair Ali Shah <syedumairalibukhari@gmail.com>
2026-09-14 22:39:03 +0300

    Create CtsAuthServiceApplication class for Spring Boot

---
+++ src/main/java/dev/umairalishah/ctsauth/CtsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.umairalishah.ctsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtsAuthServiceApplication.class, args);
+    }
+}
+
