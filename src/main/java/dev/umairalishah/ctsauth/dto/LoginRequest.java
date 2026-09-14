618fc08e406214dbfb0dddf7b2c6774da005445e
Umair Ali Shah <syedumairalibukhari@gmail.com>
2026-09-14 22:43:40 +0300

    Add LoginRequest DTO with validation annotations

---
+++ src/main/java/dev/umairalishah/ctsauth/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package dev.umairalishah.ctsauth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record LoginRequest(
+        @NotNull @Email String email,
+        @NotNull String password
+) {
+}
+
